e96d1b7365c540b2d8c5532a7cfd0d8d47ca095f
Jahnavi Chandaluri <jahnavi.chandaluri@gmail.com>
2020-04-11 20:08:28 +0530

    updated rest api and jquery based category and complaint pages

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask , render_template
-from db_model import Complaint,Response,Category,db,app
+from flask import Flask , render_template,request, jsonify
+from flask_restful import Resource,Api
+from db_model import Complaint, ComplaintSchema, Response, Category, CategorySchema, db, app
 #app = Flask(__name__)
+api = Api(app)
+
 
 @app.route("/")
 def hello():
@@ -13,3 +16,84 @@ def home():
     for complaint in complaints:
         print(complaint.subject)
     return render_template('index.html', title='Home', user=user, complaints=complaints)
+
+
+class CategoryList(Resource):
+    def get(self):
+        categories = Category.query.all()
+        return {'category' : CategorySchema(many=True).dump(categories) }
+        
+    def post(self):
+        name = request.json['name']
+        category = Category(name = name)
+        db.session.add(category)
+        db.session.commit()
+        return { 'category' : CategorySchema().dump(category) }
+
+api.add_resource(CategoryList, '/api/category')
+
+class CategoryItem(Resource):
+    def get(self,id):
+        category = Category.query.filter_by(id=id).first()
+        return { 'category' : CategorySchema().dump(category) }
+    def delete(self, id):
+        category = Category.query.filter_by(id=id).first()
+        db.session.delete(category)
+        db.session.commit()
+        return { 'category' : CategorySchema().dump(category) }
+    
+    def put(self, id):
+        category = Category.query.filter_by(id=id).first()
+        name = request.json['name']
+        category.name = name
+        db.session.commit()
+        return { 'category' : CategorySchema().dump(category) }
+    
+        
+
+
+
+api.add_resource(CategoryItem, '/api/category/<string:id>')
+
+class ComplaintList(Resource):
+    def get(self):
+        complaints = Complaint.query.all()
+        return { 'complaint' : ComplaintSchema(many=True).dump(complaints) }
+    
+    def post(self):
+        subject = request.json['subject']
+        description = request.json['description']
+        category_id =request.json['category_id']
+        submitted_by = request.json['submitted_by']
+        complaint = Complaint(subject = subject,description = description, category_id = category_id,submitted_by = submitted_by)
+        db.session.add(complaint)
+        db.session.commit()
+        return { 'complaint' : ComplaintSchema().dump(complaint) }
+
+
+api.add_resource(ComplaintList, '/api/complaint')
+
+class ComplaintItem(Resource):
+    def get(self, id):
+        complaint = Complaint.query.filter_by(id=id).first()
+        return { 'complaint' : ComplaintSchema().dump(complaint) }
+    
+    def delete(self, id):
+        complaint = Complaint.query.filter_by(id=id).first()
+        db.session.delete(complaint)
+        db.session.commit()
+        return { 'complaint' : ComplaintSchema().dump(complaint) }
+    
+    def put(self, id):
+        complaint = Complaint.query.filter_by(id=id).first()
+        subject = request.json['subject']
+        complaint.subject = subject
+        db.session.commit()
+        return { 'complaint' : ComplaintSchema().dump(complaint) }
+    
+    
+    
+
+
+api.add_resource(ComplaintItem, '/api/complaint/<string:id>')
+
--- db_model.py
+++ db_model.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema,fields, ValidationError,pre_load
 import logging 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='web')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://jahnavi:chandaluri@localhost/cms'
 db = SQLAlchemy(app)
-
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,9 +13,9 @@ logging.basicConfig(level=logging.INFO)
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_date = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(255) , unique=True)
     complaints = db.relationship('Complaint', backref ='category', lazy=True, cascade="all, delete-orphan")
-    
+
 
 class Complaint(db.Model):
     id= db.Column(db.Integer, primary_key=True)
@@ -26,6 +26,20 @@ class Complaint(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     responses= db.relationship('Response', backref ='complaint', lazy=True, cascade="all, delete-orphan")
     
+class ComplaintSchema(Schema):
+        id = fields.Integer()
+        created_date = fields.Date()
+        subject = fields.String()
+        description = fields.String()
+        submitted_by = fields.String()
+        category_id = fields.Integer()
+
+class CategorySchema(Schema):
+        id = fields.Integer()
+        created_date = fields.Date()
+        name = fields.String()
+        complaints = fields.List(fields.Nested(ComplaintSchema()))
+
 
 class Response(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -61,7 +75,7 @@ db.session.commit()
 test = Complaint.query.first()
 print(test.subject)
 
-cat1=Category.query.filter_by(id=test.category_id).all()
-print(len(cat1))
-cat = Category.query.first()
-print(cat.complaints[0].subject)
+#cat1=Category.query.filter_by(id=test.category_id).all()
+#print(len(cat1))
+#cat = Category.query.first()
+#print(cat.complaints[0].subject)
